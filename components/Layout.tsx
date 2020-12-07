5b2b2b6298389067ce6f7d73946c4a90b956cf98
Ahmed Rezk <ahmedrezk@Ahmeds-MacBook-Pro.local>
2020-12-06 21:08:27 -0700

    ADD: fonts.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { Layout as AntLayout } from 'antd';
 import Sidebar from '@/components/Sidebar';
+import Navbar from '@/components/Navbar';
 const { Content } = AntLayout;
 
 type Props = {
@@ -16,29 +17,30 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
             <title>{title}</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                rel="stylesheet"
+            />
+            <link
+                href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap"
+                rel="stylesheet"
+            />
         </Head>
+
         <AntLayout style={{ minHeight: '100vh' }}>
-            {/* <header>
-                <nav>
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="/about">
-                        <a>About</a>
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="/users">
-                        <a>Users List</a>
-                    </Link>{' '}
-                    | <a href="/api/users">Users API</a>
-                </nav>
-            </header> */}
-            <Sidebar collapsed={false} />
+            {/* Sidebar  */}
+            <Sidebar />
+
             <AntLayout>
+                {/* Navbar */}
+                <Navbar />
+
+                {/* Content */}
                 <Content style={{ marginTop: 30 }}>
                     <div style={{ padding: 24, minHeight: 360 }}> {children}</div>
                 </Content>
+
+                {/* Footer */}
                 <footer>
                     <hr />
                     <span>I'm here to stay (Footer)</span>

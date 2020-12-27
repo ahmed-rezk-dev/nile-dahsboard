06c9dda0c18edf5c48df044f035b5a5dbc0d50ed
Ahmed Rezl <work72019@gmail.com>
2020-12-26 18:08:31 -0700

    UPDATE: refresh token generate

--- utils/generateToken.ts
+++ utils/generateToken.ts
@@ -1,11 +1,18 @@
+import { genSalt, hash } from 'bcryptjs';
 import { User } from 'generated/graphql';
 import { decode, sign } from 'jsonwebtoken';
-import { ACCESS_TOKEN_EXPIRES, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_EXPIRES, REFRESH_TOKEN_SECRET } from './config';
+import { v4 as uuidv4 } from 'uuid';
+import { ACCESS_TOKEN_EXPIRES, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_EXPIRES } from './config';
 
 export interface Decode {
     userId: string | undefined;
     iat: number;
     exp: number;
+}
+
+export interface RefreshToken {
+    token: string;
+    exp: string;
 }
 
 export const generateAccessToken = (user: User) => {
@@ -15,11 +22,16 @@ export const generateAccessToken = (user: User) => {
     return token;
 };
 
-export const generateRefreshToken = () => {
-    const token = sign({}, REFRESH_TOKEN_SECRET, {
-        expiresIn: parseInt(REFRESH_TOKEN_EXPIRES as string),
-    });
-    return token;
+export const generateRefreshToken = async (): Promise<RefreshToken> => {
+    const token = uuidv4();
+    const toMilliseconds = (REFRESH_TOKEN_EXPIRES as number) * 1000;
+    const exp = new Date(Date.now() + toMilliseconds).getTime().toString();
+    console.log('🛎 exp: => ', exp);
+
+    // const salt = await genSalt(10);
+    // const token = await hash(refreshToken, salt);
+
+    return { token, exp };
 };
 
 export const decodeToken = (token: string) => {

7ee1d97707e4eea2e9b41ea97ad1ef0c9b90a0db
Ahmed Rezl <work72019@gmail.com>
2021-01-25 17:20:25 -0700

    UPDATE: support Brave browser Test Runner.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -34,7 +34,6 @@ const findBrowser = () => {
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
 /**
  * @type {Cypress.PluginConfig}
  */

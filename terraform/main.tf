92a74cc2a3913464f68e7f408a05c184fc048188
Ahmed Rezk <work72019@gmail.com>
2021-04-01 12:47:11 -0600

    Point backend to correct TFC org and workspace

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+ backend "remote" {
+    organization = "dashboardJS"
+
+    workspaces {
+      name = "dashboards"
+    }
+  }
 }
 
 # Main region where the resources should be created in
